b56666e24cd8e08332190e8c238a66532373b435
lakshaygirdhar <lakshay148@gmail.com>
2016-03-31 21:06:43 +0530

    git ignore files

--- app/src/main/java/com/truedev/neonsample/MainActivity.java
+++ app/src/main/java/com/truedev/neonsample/MainActivity.java
@@ -2,12 +2,23 @@ package com.truedev.neonsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.truedev.neonsample.adapter.BaseRecyclerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rcvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rcvItems = (RecyclerView) findViewById(R.id.rcvItems);
+
+//        BaseRecyclerAdapter
+
+
     }
 }
---
+++ app/src/main/java/com/truedev/neonsample/adapter/BaseRecyclerAdapter.java
@@ -0,0 +1,44 @@
+package com.truedev.neonsample.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lakshaygirdhar on 31/3/16.
+ */
+public class BaseRecyclerAdapter<S, T extends RecyclerView.ViewHolder> extends RecyclerView.Adapter<T> {
+
+    private Context mContext;
+    private ArrayList<S> mObjects;
+    private BindAdapterListener mListener;
+    private T mHolder;
+
+    public interface BindAdapterListener<T> {
+        public void onBind(T holder, int position);
+    }
+
+    public BaseRecyclerAdapter(Context context, ArrayList<S> objects, T holder, BindAdapterListener listener) {
+        mContext = context;
+        mObjects = objects;
+        mHolder = holder;
+        mListener = listener;
+    }
+
+    @Override
+    public T onCreateViewHolder(ViewGroup parent, int viewType) {
+        return mHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(T holder, int position) {
+        mListener.onBind(holder, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mObjects.size();
+    }
+}
